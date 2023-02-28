5754acdc572fafaf05ab6291c307f9f47e411b00
Sina <alisinashafieezada@gmail.com>
2023-02-28 16:21:46 +0330

    token check on home page

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,6 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { RootState } from '../../redux';
 
 const MainLayout = () => {
+  const token = useSelector((state: RootState) => state.rootReducer.auth.token);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token) {
+      navigate('/home', { replace: true });
+    }
+  }, [token, navigate]);
+
   return (
     <>
       <Outlet />
