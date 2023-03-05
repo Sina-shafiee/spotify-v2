363b8b154861d5911b8a30ddf46b53d9d63a0480
Sina <alisinashafieezada@gmail.com>
2023-03-05 20:58:04 +0330

    cors proxy

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -2,6 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.spotify.com/v1' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://cors-anywhere.herokuapp.com/https://api.spotify.com/v1'
+  }),
   endpoints: (builder) => ({})
 });
