57097135c3f226c4d42d01fe67d54b0d505ae5c9
Sina <alisinashafieezada@gmail.com>
2023-03-07 16:27:39 +0330

    auth cors fix

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://cors-anywhere.herokuapp.com/https://api.spotify.com/v1'
+    baseUrl: 'https://api.spotify.com/v1'
   }),
   endpoints: (builder) => ({})
 });
